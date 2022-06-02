0c624e3a4df087b293caa14956520dda1ec68a51
Pierre-Louis Giacosa <pierre-louis.giacosa@captivea.com>
2022-06-02 11:50:25 +0200

    Update __manifest__.py

--- cap_controller_marketing/__manifest__.py
+++ cap_controller_marketing/__manifest__.py
@@ -4,6 +4,6 @@
     'author': "Captivea-pgi",
     'website': "https://www.captivea.com",
     'depends': ['base', 'mass_mailing'],
-    'data': ["views/cap_mailing_form.xml",],
+    # 'data': ["views/cap_mailing_form.xml",],
     'description': 'Override the mailing controller in order to clone the unsubscribe function from email marketing to marketing automationS',
 }
