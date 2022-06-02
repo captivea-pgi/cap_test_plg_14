4760bc9e3b682cb42d22f2f50b5f91cd3bcc3fef
Pierre-Louis Giacosa <pierre-louis.giacosa@captivea.com>
2022-06-02 12:56:46 +0200

    fix

--- cap_controller_marketing/models/mailing_mailing.py
+++ cap_controller_marketing/models/mailing_mailing.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api, _
 
-class AutomatedRules(models.Model):
+class MassMailing(models.Model):
     _inherit = "mailing.mailing"
 
     mailing_list_ids = fields.Many2many('mailing.list', string = "Mailing List")
